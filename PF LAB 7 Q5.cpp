58b99afbc57d57c35162d07ab0b1cdd19444ede9
Asadullah Ishfaque <k253026@nu.edu.pk>
2025-10-17 10:39:54 +0500

    Add files via upload

---
+++ PF LAB 7 Q5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ int main() {
+ 	int arr[10] = {1,2,3,47,65,6,7,8,11,10};
+ 	int i,max=0,min=0;
+ 	
+ 	for(i=0;i<10;i++) {
+ 		if(arr[i]>max) {
+ 		max=arr[i];} 
+		 else if(
+ 		min=arr[i]
+		 );
+  	} 
+	  	printf("The maximum is %d", max);
+  	    printf("\nThe minimum is %d", min);
+	  printf("\nThe Difference is %d",max - min);
+ 	return 0;
+ }
+ 
